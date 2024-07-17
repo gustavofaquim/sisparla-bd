bbe790d1485351c499f3140451a13054fc7d9abf
Gustavo Faquim <gustavofaquim408@gmail.com>
2024-07-17 20:48:34 -0300

    feat: ajustes

--- MudancaEstruturaPermissoes.sql
+++ MudancaEstruturaPermissoes.sql
@@ -7,26 +7,22 @@ SELECT
 	u.Nome,
 	u.Senha,
 	u.PerfilAcesso AS IdPerfil,
-	pa.Nome as PerfilNome,
-	t.Nome AS Tela,
+	PA.Nome as PerfilNome,
+	T.Nome AS Tela,
 	p.IdPermissao,
 	p.Nome AS PermissaoNome
 FROM
 	USUARIO u
 	INNER JOIN PERFIL_ACESSO PA ON u.PerfilAcesso = PA.IdPerfil
     INNER JOIN PERFIL_PERMISSAO PP ON PP.IdPerfil = PA.IdPerfil
-	INNER JOIN TELA t on t.IdTela = PP.IdTela
+	INNER JOIN TELA T on T.IdTela = PP.IdTela
 	INNER JOIN PERMISSAO p ON p.IdPermissao = PP.IdPermissao
 WHERE
-	u.NomeUsuario = 'gustavofaquim'
-    AND T.Nome = 'grupos';
+	u.NomeUsuario = 'gustavofaquim';
     
     
-
-
-ALTER TABLE USUARIO DROP RegraAcesso;
-ALTER TABLE USUARIO DROP foreign key USUARIO_ibfk_1;
-
+   
+	
 
 
 SELECT * FROM PERFIL_ACESSO;
@@ -68,6 +64,10 @@ FOREIGN KEY (IdPermissao) REFERENCES PERMISSAO(IdPermissao)
 ); */
 
 
+DROP TABLE PERFIL_PERMISSAO;
+DROP TABLE PERMISSAO;
+
+
 CREATE TABLE PERMISSAO(
 IdPermissao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 Nome VARCHAR(300) NOT NULL
@@ -81,6 +81,14 @@ INSERT PERMISSAO (Nome) VALUES
 ('Emitir Relatório');
 
 
+ALTER TABLE USUARIO DROP foreign key fk_usuario_perfil;
+ALTER TABLE USUARIO DROP PERFIL;
+DROP TABLE PERFIL_ACESSO;
+DROP TABLE PERFIL;
+
+
+DROP TABLE PERFIL_PERMISSAO;
+
 CREATE TABLE PERFIL_ACESSO(
 IdPerfil INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 Nome VARCHAR(200) NOT NULL
@@ -89,9 +97,10 @@ Nome VARCHAR(200) NOT NULL
 INSERT PERFIL_ACESSO (Nome) 
 VALUES 
 ('Administrador'),
-('Chefia de Gabinete'),
-('Assessoria Padrão'),
-('Assessoria Cadastramento');
+('Chefe de Gabinete'),
+('Assessor Padrão '),
+('Assessor Cadastro');
+
 
 CREATE TABLE PERFIL_PERMISSAO(
 IdPerfil INT,
@@ -104,17 +113,34 @@ FOREIGN KEY (IdPermissao) REFERENCES PERMISSAO(IdPermissao)
 );
 
 
-INSERT PERFIL_PERMISSAO VALUES 
-(1, 1, 1);
 
 
-ALTER TABLE USUARIO DROP foreign key fk_usuario_perfil;
-DROP TABLE PERFIL_ACESSO;
+-- INSERT PERFIL_PERMISSAO
+SELECT 
+	PA.IdPerfil,
+	T.IdTela,
+	P.IdPermissao
+	-- * 
+FROM 
+	PERFIL_ACESSO PA
+	INNER JOIN TELA T ON T.IdTela = T.IdTela
+	INNER JOIN PERMISSAO P ON P.IdPermissao  = P.IdPermissao
+WHERE PA.IdPerfil = 3
+ORDER BY PA.IdPerfil, T.IdTela, P.IdPermissao 
+
+
+
 
 ALTER TABLE USUARIO DROP Perfil;
 ALTER TABLE USUARIO ADD PerfilAcesso INT;
 ALTER TABLE USUARIO ADD CONSTRAINT fk_regra_acesso FOREIGN KEY (PerfilAcesso) REFERENCES PERFIL_ACESSO(IdPerfil);
 
 
+
+
+
+UPDATE USUARIO 
+SET PerfilAcesso = 4
+WHERE IdUsuario  = 6;
 
 
